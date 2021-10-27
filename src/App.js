ae5d0783ad2923b5b81881e953e04a834d1ae165
jpickard393 <jpickard@and.digital>
2021-10-27 11:56:08 +0100

    Basic Context example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+// create context and put the result in a vaklriable called VehicleContext
+export const VehicleContext = React.createContext();
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Use Vehicle context.provider to wrap components that will use it */}
+      {/* Set initial value */}
+      <VehicleContext.Provider value="Peugeot 207">
+        <Vehicle />
+      </VehicleContext.Provider>
     </div>
   );
 }
+
+
+const Vehicle = () => {
+  return (
+    <VehicleContext.Consumer>
+      {/* Use the passed down value from the Vehicle context */}
+      {value => <h1>{value}</h1>}
+    </VehicleContext.Consumer>
+  );
+};
 
 export default App;
